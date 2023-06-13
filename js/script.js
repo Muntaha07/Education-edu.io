5e5f1389db3e48d557e9d244815ceac503f1433a
Muntaha07 <beighmuntaha7@gmail.com>
2023-06-13 11:16:09 +0530

    edu project website

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+  loginForm.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+  navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
+}
+
+var swiper = new Swiper(".review-slider",{
+  spaceBetween: 20,
+  centeredSlides: true,
+  grabCursor: true,
+  autoplay:{
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
